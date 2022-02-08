b9fa311c1678f58151726ba0564ca3887fb499ba
joel.pettersson90@gmail.com <joel.pettersson90@gmail.com>
2022-02-08 14:18:25 +0100

    added all th einputs and meda it done

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  textFromFirstInput: any = ''
+  textFromSecondInput: any = ''
+  textFromThirdInput: any = ''
+
+  firstInputValue(text: string) {
+    this.textFromFirstInput = text
+  }
+
+  secondInputValue(secondInput: KeyboardEvent) {
+    
+    console.log(KeyboardEvent);
+
+    this.textFromSecondInput = secondInput
+
+  }
+
+  thirdInputValue(text: string) {
+    this.textFromThirdInput = text
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
